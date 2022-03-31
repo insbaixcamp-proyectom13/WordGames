11318159f4953e2c6692e3e1ba1fac369f79e645
juanjose <juan.guevara@insbaixcamp.cat>
2022-03-31 16:45:12 +0200

    Registro

---
+++ app/src/main/java/com/insbaixcamp/game/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.insbaixcamp.game
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.insbaixcamp.game.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+    private var currentUser: FirebaseUser ? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        currentUser = auth.currentUser ?: signInAnonymously()
+    }
+
+    private fun signInAnonymously() : FirebaseUser? {
+        auth.signInAnonymously().addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d("TAG", "signInAnonymously:success")
+                currentUser = auth.currentUser!!
+//                updateUI(user)
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.w("TAG", "signInAnonymously:failure", task.exception)
+                Toast.makeText(baseContext, "Authentication failed.",
+                    Toast.LENGTH_SHORT).show()
+//                updateUI(null)
+                return@addOnCompleteListener null!!
+            }
+        }
+
+        return currentUser
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        TODO("Not yet implemented")
+    }
+
+}
