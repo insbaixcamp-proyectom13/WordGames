6b0d5e8e35714948e0df105f94d51fb5a6dc7a65
juanjose <juanjose.99.alvarez@gmail.com>
2022-04-16 06:24:53 +0200

    UI wordle(clicks sobre el teclado)

--- app/src/main/java/com/insbaixcamp/game/ui/wordle/WordleFragment.kt
+++ app/src/main/java/com/insbaixcamp/game/ui/wordle/WordleFragment.kt
@@ -2,17 +2,17 @@ package com.insbaixcamp.game.ui.wordle
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.TextView
-import com.insbaixcamp.game.R
-import com.insbaixcamp.game.databinding.FragmentNotificationsBinding
 import com.insbaixcamp.game.databinding.FragmentWordleBinding
 import com.insbaixcamp.game.ui.notifications.NotificationsViewModel
 
-class WordleFragment : Fragment() {
+class WordleFragment : Fragment(), OnClickListener {
     private var _binding: FragmentWordleBinding? = null
 
     // This property is only valid between onCreateView and
@@ -20,6 +20,7 @@ class WordleFragment : Fragment() {
     private val binding get() = _binding!!
     private var input = arrayOf(charArrayOf())
     private var word = charArrayOf()
+    private lateinit var root: View
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,7 +31,19 @@ class WordleFragment : Fragment() {
             ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
         _binding = FragmentWordleBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        root = binding.root
+        var container1 = binding.incKeyboard.linearLayout as ViewGroup
+        var container2 = binding.incKeyboard.linearLayout2 as ViewGroup
+        var container3 = binding.incKeyboard.linearLayout3 as ViewGroup
+
+        Log.i("butons", container1.childCount.toString())
+        Log.i("butons", container2.childCount.toString())
+        Log.i("butons", container3.childCount.toString())
+
+        for (i in 0 until container1.childCount){
+            var v = container1.getChildAt(i)
+            v.setOnClickListener(this)
+        }
 
         return root
     }
@@ -38,6 +51,18 @@ class WordleFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onClick(p0: View?) {
+        var butn = p0 as TextView
+        Log.i("click", butn.text.toString())
+
+//        if (p0 == root.findViewById(R.id.inc_keyboard)){
+//            var layout = p0 as LinearLayout?
+//            if (layout != null) {
+//
+//            }
+//        }
     }
 
 }
