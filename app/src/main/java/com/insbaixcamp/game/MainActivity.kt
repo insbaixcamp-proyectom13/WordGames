410986ed14abf5045901f1d7967a9e7ed9d38fe7
Alumne <damproyectom13@gmail.com>
2022-04-26 19:40:54 +0200

    Empezando UI

--- app/src/main/java/com/insbaixcamp/game/MainActivity.kt
+++ app/src/main/java/com/insbaixcamp/game/MainActivity.kt
@@ -2,21 +2,27 @@ package com.insbaixcamp.game
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.insbaixcamp.game.databinding.ActivityMainBinding
 import com.insbaixcamp.game.utilities.Diccionaro
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,17 +30,28 @@ class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private var currentUser: FirebaseUser ? = null
 
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = Firebase.auth
 
         Diccionaro.inicialitzar(resources, getShared(), packageName)
 
+        //Adding custom top appBar layout
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+        supportActionBar!!.setDisplayShowCustomEnabled(true)
+        var infla = this.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        var view = infla.inflate(R.layout.custom_appbar, null) as View
+        supportActionBar!!.customView = view
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Customizing bottom navigaton bar
         val navView: BottomNavigationView = binding.navView
-
+        val menuView = binding.navView.getChildAt(0) as BottomNavigationMenuView
+        val iconView = menuView.getChildAt(0).findViewById<View>(R.id.navigation_home) as View
+        iconView.setLeftTopRightBottom(-14, 0, 0, 0)
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -46,7 +63,16 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-//        Log.i("langMAIN", Diccionaro.findLanguage())
+//        setLanguage("ru")
+    }
+
+    private fun setLanguage(upLanguage: String) {
+        val config = resources.configuration
+        val locale = Locale(upLanguage)
+        Locale.setDefault(locale)
+
+        config.locale = locale
+        resources.updateConfiguration(config, resources.displayMetrics)
     }
 
     //    private fun loadDictionary(lang:String) {
@@ -100,6 +126,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         return currentUser
+    }
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(newBase)
     }
 
     private fun updateUI(user: FirebaseUser?) {
